90bdd33ff106e87eeb41ec5f8cb0edf6aecc2a53
Jenifer Castaño Ledesma <jennifercasled@gmail.com>
2024-11-09 23:43:37 -0500

    add disable account functionality

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import UserService from "../services/user/user.services";
 import createError from "http-errors";
+import AuthService from "../services/user/auth.services";
+import userServices from "../services/user/user.services";
 
 class UserController {
   async createUser(req: Request, res: Response, next: NextFunction) {
@@ -32,6 +34,35 @@ class UserController {
     } catch (error) {
       next(createError(400, (error as Error).message));
     }
+  }
+
+  async disableAccount(req: Request, res: Response, next: NextFunction) {
+    try {
+      
+      const { token } = req.params;
+      const { password } = req.body;
+
+      const tokenUser = await AuthService.verifyToken(token);
+      const user = await UserService.getUserById(Number(tokenUser.id));
+
+      if (!user) {
+        throw createError(401, "Usuario no encontrado");
+      }
+
+      const bdPassword = await UserService.getpasswordById(tokenUser.id);
+      const isCorrectPassword = await AuthService.comparePasswords(password, bdPassword);
+
+      if(!isCorrectPassword) {
+        throw createError(401, "Contraseña incorrecta");
+      }
+
+      await userServices.disableAccount(tokenUser.id);
+      
+      res.status(200).json(user);
+    } catch (error) {
+      next(createError(400, (error as Error).message));
+    }
+
   }
 }
 
